536dec1c5e053760e9631a0def4deb0afb2451cd
Frans Dekkers <fde.gma@gmail.com>
2018-01-23 23:03:20 +0100

    Eerste probeersel om gescript een model aan te maken. Werkt (nog) niet.

--- server/bin/autodisc2.js
+++ server/bin/autodisc2.js
@@ -4,27 +4,56 @@ const SqlStatusPart = 1; //Though return ed in the resultset from SQL, not used 
 
 var path = require('path');
 var fs = require('fs');
-var app = require(path.resolve(__dirname, '../server'));
+var lpbk = require('loopback');
+//var app = require(path.resolve(__dirname, '../server'));
 var outputPath = path.resolve(__dirname, '../../common/models');
-var ds = app.dataSources.TestDS;
+//var ds = app.dataSources.TestDS;
 var sql = "CALL API_GetAPItoDBDetails()";
+//var exec = require('child_process').execFile;
 
-ds.connector.query(sql, function(err, ReturnedData) {
-    if (err) {
-        console.log("Error:", err);
-    }
-    Records = ReturnedData[SqlDataPart];
-    console.log("------------------------------------------------------------");
-    for (var aRecord in Records) {
-        console.log("---> CompletedOk = ", Records[aRecord].CompletedOk);
-        console.log("---> APItoDBRoute = ", Records[aRecord].APItoDBRoute);
-        console.log("---> SpoFieldNamesAndTypes = ");
-        Parms = JSON.parse(Records[aRecord].SpoFieldNamesAndTypes);
-        for (var aField in Parms) {
-            console.log("    ---> Parm Name = ", Parms[aField].Name);
-            console.log("    ---> Parm Type = ", Parms[aField].type);
-            console.log("    ------------------------------------------------ ");
+
+lpbk.createModel({
+    name: 'Author',
+    properties: {
+        firstName: 'string',
+        lastName: 'string'
+    },
+    relations: {
+        books: {
+            model: 'Book',
+            type: 'hasAndBelongsToMany'
         }
-        console.log("--------------------------------------------------------");
     }
 });
+
+
+// var fun = function() {
+//     console.log("fun() start");
+//     exec('note.exe', 'test.tst', function(err, data) {
+//         console.log(err)
+//         console.log(data.toString());
+//     });
+// }
+
+
+// ds.connector.query(sql, function(err, ReturnedData) {
+//     if (err) {
+//         console.log("Error:", err);
+//     }
+//     Records = ReturnedData[SqlDataPart];
+//     console.log("------------------------------------------------------------");
+//     for (var aRecord in Records) {
+//         console.log("---> CompletedOk = ", Records[aRecord].CompletedOk);
+//         console.log("---> APItoDBRoute = ", Records[aRecord].APItoDBRoute);
+//         console.log("---> SpoFieldNamesAndTypes = ");
+//         Parms = JSON.parse(Records[aRecord].SpoFieldNamesAndTypes);
+//         for (var aField in Parms) {
+//             console.log("    ---> Parm Name = ", Parms[aField].Name);
+//             console.log("    ---> Parm Type = ", Parms[aField].type);
+//             console.log("    ------------------------------------------------ ");
+//         }
+//         console.log("--------------------------------------------------------");
+//     }
+// });
+
+// fun();
